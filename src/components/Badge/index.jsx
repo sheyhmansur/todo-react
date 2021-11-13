fd9e0482e5bfa02333fa5926b0f0e3d2912c96dc
Sheyh-Mansur Dzhavathanov <92082169+sheyhmansur@users.noreply.github.com>
2021-11-13 16:25:00 +0300

    lesson 2 done!

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 import "./Badge.scss";
 
-const Badge = ({ color }) => <i className={`badge badge--${color}`}></i>;
+// `badge badge--${color} ${className}`;
+
+const Badge = ({ color, onClick, className }) => (
+  <i
+    onClick={onClick}
+    className={classNames("badge", { [`badge--${color}`]: color }, className)}
+  ></i>
+);
 
 export default Badge;
